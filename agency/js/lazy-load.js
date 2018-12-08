b540251f42bdad39573ef9a3c1e2ff7d41f71d55
Jared Blumer <jaredgblumer@gmail.com>
2018-12-08 17:16:53 -0500

    Add lazy loading for images

--- agency/js/lazy-load.js
+++ agency/js/lazy-load.js
@@ -1,12 +1,30 @@
-// Lazy Load Div Background Images
+// Lazy Load Script
 let options = {
   rootMargin: '500px',
 };
 
 document.addEventListener("DOMContentLoaded", function() {
   let lazyBackgrounds = [].slice.call(document.querySelectorAll(".lazy-background"));
+  let lazyImages = [].slice.call(document.querySelectorAll("img.lazy-image"));
   
   if("IntersectionObserver" in window) {
+    // Lazy Load Images
+    let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.classList.remove("lazy-image");
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
+    }, options);
+
+    lazyImages.forEach(function(lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+    
+    // Lazy Load Div Background Images
     let lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
       entries.forEach(function(entry) {
         if (entry.isIntersecting) {
